ac94988787cfb666a855c0c56d253614552fa91c
binlihpu <binlihpu@gmail.com>
2019-11-19 10:58:11 +0800

    init

---
+++ leaf.go
@@ -0,0 +1,46 @@
+package leaf
+
+import (
+	"github.com/binlihpu/leaf/cluster"
+	"github.com/binlihpu/leaf/conf"
+	"github.com/binlihpu/leaf/console"
+	"github.com/binlihpu/leaf/log"
+	"github.com/binlihpu/leaf/module"
+	"os"
+	"os/signal"
+)
+
+func Run(mods ...module.Module) {
+	// logger
+	if conf.LogLevel != "" {
+		logger, err := log.New(conf.LogLevel, conf.LogPath, conf.LogFlag)
+		if err != nil {
+			panic(err)
+		}
+		log.Export(logger)
+		defer logger.Close()
+	}
+
+	log.Release("Leaf %v starting up", version)
+
+	// module
+	for i := 0; i < len(mods); i++ {
+		module.Register(mods[i])
+	}
+	module.Init()
+
+	// cluster
+	cluster.Init()
+
+	// console
+	console.Init()
+
+	// close
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	log.Release("Leaf closing down (signal: %v)", sig)
+	console.Destroy()
+	cluster.Destroy()
+	module.Destroy()
+}
